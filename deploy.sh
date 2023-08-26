03412a05585e1f3b1a7b4483497f21ca7dcdac64
soumyarepo <ranjan.soumya8055@gmail.com>
2023-08-26 22:00:32 +0530

    add kubernetesfile

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to the Kubernetesmanifests folder
-MANIFESTS_FOLDER="Kubernetesmanifests"
+MANIFESTS_FOLDER="kubernetesmanifests"
 
 # Loop through each YAML file in the folder and apply it using kubectl
 for file in "$MANIFESTS_FOLDER"/*.yaml; do
